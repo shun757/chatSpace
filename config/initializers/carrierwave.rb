d8c02edf692f662a5d17bb2e48fc154d0ed4ce8c
shun757 <shunsuke.sakurai@di-v.co.jp>
2019-07-16 17:57:11 +0900

    自動デプロイ の編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'shunsuke1257'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/shunsuke1257'
+  config.fog_directory  = 'sschat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sschat-space'
 end
