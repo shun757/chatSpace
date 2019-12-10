aa864fc43c2a72f86b2dc6c5765f7488083bbc34
shun757 <shunsuke.sakurai@di-v.co.jp>
2019-12-10 13:38:43 +0900

    編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.messages.new(message_params)
+    @message = Message.new(message_params)
     if @message.save
       respond_to do |format|
         format.html
